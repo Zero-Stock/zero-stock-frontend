eee7b9e64913f52942e5ba0e34f863cfaf2107a2
hzc011213 <hzc011213@gmail.com>
2026-05-09 01:10:12 -0400

    chore: remove all existing unit tests from the project codebase

--- src/shared/test/renderWithProviders.tsx
+++
@@ -1,9 +0,0 @@
-import type { ReactElement } from 'react';
-import { render } from '@testing-library/react';
-import { LanguageProvider } from '@/shared/translation/LanguageContext';
-
-export function renderWithProviders(ui: ReactElement) {
-  localStorage.setItem('locale', 'en');
-
-  return render(<LanguageProvider>{ui}</LanguageProvider>);
-}
